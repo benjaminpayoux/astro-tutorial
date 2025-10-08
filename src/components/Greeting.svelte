0f42dccd2f02c487954ab925eea4befa7dd366c2
Benjamin Payoux <1423027+benjaminpayoux@users.noreply.github.com>
2025-10-08 12:38:22 +0200

    add svelte and theme toggle

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let { messages } = $props();
+
+  let message = $state(messages[0]);
+
+  const randomMessage = () => {
+    let newMessage = messages[Math.floor(Math.random() * messages.length)];
+    while (newMessage === message) {
+      newMessage = messages[Math.floor(Math.random() * messages.length)];
+    }
+    message = newMessage;
+  };
+</script>
+
+<h3>{message} Thanks for visiting!</h3>
+<button onclick={randomMessage}>New Greeting</button>
